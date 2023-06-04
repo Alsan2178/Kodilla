756656d557af3748de90c7625d2603ef94c7f892
Aleksander Oskroba <alsan2178@gmail.com>
2023-06-04 12:27:39 +0200

    Update palidromy

--- palindromy.py
+++ palindromy.py
@@ -1,1 +1,9 @@
-print('palindromy')
+def cheecking(word):
+    i=0
+    while i != (len(word)-i-1):
+        if word[i] != word[-i-1]:
+            return False
+        else:
+            i=i+1
+    return True
+print(cheecking('kajak'))
---
+++ test.py
@@ -0,0 +1,4 @@
+def test(p):
+    return True
+
+print(test(1))
