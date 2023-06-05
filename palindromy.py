3ecbf71233174405eee680f033f6493fce9f689a
Aleksander Oskroba <alsan2178@gmail.com>
2023-06-05 20:29:46 +0200

    Fix even-number words

--- palindromy.py
+++ palindromy.py
@@ -1,9 +1,18 @@
 def cheecking(word):
     i=0
-    while i != (len(word)-i-1):
-        if word[i] != word[-i-1]:
-            return False
-        else:
-            i=i+1
-    return True
+    if len(word)%2==1:
+        while i!=(len(word)-i-1):
+            if word[i] != word[-i-1]:
+                return False
+            else:
+                i=i+1
+        return True
+    else:
+        while i!=(len(word)-i):
+            print(len(word)-i)
+            if word[i] != word[-i-1]:
+                return False
+            else:
+                i=i+1
+        return True
 print(cheecking('kajak'))
